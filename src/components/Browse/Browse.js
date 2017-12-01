61479bbc92e646173409273032c2c8b2dad36426
Travis Bates <travbates93@gmail.com>
2017-12-01 12:33:29 -0700

    Lunch Break

--- src/components/Browse/Browse.js
+++ src/components/Browse/Browse.js
@@ -1,16 +1,29 @@
 import React, { Component } from "react"
 import "./Browse.css"
 
+import NavBar from "../NavBar/NavBar"
+
 class Browse extends Component {
-    constructor(){
+    constructor() {
         super()
     }
 
-    render(){
+    render() {
         return (
             <div>
-
-                <h1>Browse</h1>
+                <NavBar />
+                <div className="browseContainer">
+                    <div className="inventoryContainer">
+                        <div className="titleOptions">
+                            <span className="browseTitle">Browse Inventory</span>
+                            <div className="filter">
+                                <span>In Stock</span>
+                                <span>Out of Stock</span>
+                            </div>
+                            <div className="genre"></div>
+                        </div>
+                    </div>
+                </div>
 
             </div>
         )
--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -1,13 +1,23 @@
 import React, { Component } from "react"
-import {Link} from "react-router-dom"
+import { Link } from "react-router-dom"
+
+import logo from "../../assets/tan-logo.svg"
 
 import "./NavBar.css"
 
 export default class NavBar extends Component {
-    render(){
+    render() {
         return (
             <header>
-                
+                <div className="links">
+                    <img src={logo} className="logoT" />
+                    <Link to='' className="link">Browse</Link>
+                    <Link to='' className="link">Cart</Link>
+                    <Link to='' className="link">My Shelf</Link>
+                </div>
+                <div>
+                    <a className="logout" href={process.env.REACT_APP_LOGOUT}><button className="logout">Logout</button></a>
+                </div>
             </header>
         )
     }
